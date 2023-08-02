a53dad83181e7460eb9e9cb3dd58d1148223b829
lightmon233 <3535888142@qq.com>
2023-08-02 17:49:57 +0800

    markdown finished

--- markdownblog/blog/views.py
+++ markdownblog/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+
+def index(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', {'posts': posts})
+
+def detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/detail.html', {'post': post})
--- markdownblog/markdownblog/urls.py
+++ markdownblog/markdownblog/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('<int:pk>', views.detail, name='detail'),
 ]
